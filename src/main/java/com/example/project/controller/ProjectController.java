7080d8102c8da1951f96cc8230e69b54bd95834b
Ayaanle A. Hassan <ayaanle2899@gmail.com>
2022-02-28 22:53:32 +0100

    commit 1

---
+++ src/main/java/com/example/project/controller/ProjectController.java
@@ -0,0 +1,23 @@
+package com.example.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProjectController {
+
+  @GetMapping("/index")
+  public String index(){
+    return "index";
+  }
+
+  @GetMapping("/page1")
+  public String page1(){
+    return "page1";
+  }
+
+  @GetMapping("/page2")
+  public String page2(){
+    return "page2";
+  }
+}
